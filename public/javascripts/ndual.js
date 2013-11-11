cd7e6830255aebe2489797836439c1c1a7ee9301
youyudehexie <youyudehexie@gmail.com>
2013-11-11 22:43:21 +0800

    init

---
+++ public/javascripts/ndual.js
@@ -0,0 +1,60 @@
+function Ndual(cb){
+
+    var self = this;
+
+    function Widget(widgetUrl){
+        var widget = document.createElement('iframe') 
+        
+        widget.setAttribute("style", "border: none; position:fixed; z-index:10000; right:0px; top:90px;");
+        widget.setAttribute("id", "remotes-widget");
+        widget.setAttribute("scrolling", "no");
+        widget.setAttribute("src", widgetUrl); 
+        document.body.appendChild(widget);
+    }
+
+
+    function randomString(length) {
+        var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
+        
+        if (! length) {
+            length = Math.floor(Math.random() * chars.length);
+        }
+        
+        var str = '';
+        for (var i = 0; i < length; i++) {
+            str += chars[Math.floor(Math.random() * chars.length)];
+        }
+        return str;
+    }
+
+    var token = randomString(5) 
+    function createWdiget(token){
+        var widgetUrl = 'http://ndual.whattoc.com/widget?token=' + token;
+        Widget(widgetUrl);
+    }
+
+    createWdiget(token);
+    var iosocket = io.connect('http://ndual.whattoc.com:80');
+
+    iosocket.on('connect', function () {
+        console.log('connected');
+    });
+
+    iosocket.emit('register', {
+        token: token
+    }, function(){
+        console.log('register')
+    })
+
+    iosocket.on('message', function(message) {
+        if(message == 'logined'){
+            var widget = document.getElementById("remotes-widget"); 
+            if(widget){ 
+                document.body.removeChild(widget)
+            }
+        }
+        cb(message)
+    });
+}
+
+
